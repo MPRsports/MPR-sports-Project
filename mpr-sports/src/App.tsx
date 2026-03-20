bfda7a7fad401bb8d8eb1ab56e265afd53fd3d61
Alex Caires <66693497+AleexCaires@users.noreply.github.com>
2026-03-20 18:37:27 +0100

    Revert "Cloudflare/workers autoconfig 2 (#6)"
    This reverts commit 8283f3825ecad561024e2e337165dc6476f0c205.

--- mpr-sports/src/App.tsx
+++ mpr-sports/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="coming-soon-root">
       <div className="coming-soon-card">
-        <h1 className="brand">MPR SPORTS(STAGING)</h1>
+        <h1 className="brand">MPR SPORTS</h1>
         <h2 className="tag">COMING SOON</h2>
       </div>
     </div>
